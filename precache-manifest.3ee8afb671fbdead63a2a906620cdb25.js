3f0ad91c1ab1591d9b41126d909699eb176671e4
chizoba <chizyberto@gmail.com>
2019-12-28 12:41:21 +0100

    Updates

---
+++ precache-manifest.3ee8afb671fbdead63a2a906620cdb25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dce12129d0a742cfe0916a1ef5f49b00",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea6f2f628ff8655cdcba",
+    "url": "/static/css/main.3754efb2.chunk.css"
+  },
+  {
+    "revision": "48a882fb59eaaa7d425d",
+    "url": "/static/js/2.42b7e9c7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.42b7e9c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea6f2f628ff8655cdcba",
+    "url": "/static/js/main.0bbac995.chunk.js"
+  },
+  {
+    "revision": "70f7d5e24e1ec37e7438",
+    "url": "/static/js/runtime-main.df45f94f.js"
+  }
+]);
